217a3894f2de38850a67eda84abbc6c7356917af
Abhinav Anand <abhinav_anand@live.com>
2019-07-01 10:42:13 +0530

    Add zsh configuration commands

--- home/abnv/scripts/config.sh
+++ home/abnv/scripts/config.sh
@@ -1,3 +1,9 @@
+# configure git
 git config --global user.email "abhinav_anand@live.com"
 git config --global user.name "Abhinav Anand"
 git config --global credential.helper 'cache --timeout=43200'
+
+# configure shell
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
